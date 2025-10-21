192ec8ca9e66c99523a927c6e1a3b96e5fcd76ac
Samioul51 <akmsamioulislam51@gmail.com>
2025-10-22 00:00:53 +0600

    Created JSON data and ErrorPage

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -10,29 +10,29 @@ const Navbar = () => {
                 <div className='flex gap-[4px] justify-center items-center cursor-pointer'>
                     <NavLink to="/">
                         <img src={logo} className="w-full h-[40px]" />
-                        <p className='font-bold text-[16px] bg-gradient-to-r from-[#632EE3] to-[#9F62F2] bg-clip-text text-transparent'>GameHub</p>
+                        <p className='font-bold text-[16px] bg-linear-to-r from-[#632EE3] to-[#9F62F2] bg-clip-text text-transparent'>GameHub</p>
                     </NavLink>
                 </div>
                 <ul className="w-full max-w-[70%] flex flex-col justify-center items-center gap-[10px] lg:flex-row lg:gap-[32px] lg:justify-end">
                     <NavLink to="/" className={({ isActive }) => `
-                    relative text-[16px] cursor-pointer font-medium ${isActive ? "bg-gradient-to-r from-[#632EE3] to-[#9F62F2] bg-clip-text text-transparent after:content-[''] after:absolute after:left-0 after:bottom-0 after:w-full after:h-[1px] after:bg-gradient-to-r after:from-[#632EE3] after:to-[#9F62F2]" : "text-black"}
+                    relative text-[16px] cursor-pointer font-medium ${isActive ? "bg-linear-to-r from-[#632EE3] to-[#9F62F2] bg-clip-text text-transparent after:content-[''] after:absolute after:left-0 after:bottom-0 after:w-full after:h-px after:bg-linear-to-r after:from-[#632EE3] after:to-[#9F62F2]" : "text-black"}
                 `}>
                         Home
                     </NavLink>
 
                     <NavLink to="/apps" className={({ isActive }) => `
-                    relative text-[16px] cursor-pointer font-medium ${isActive ? "bg-gradient-to-r from-[#632EE3] to-[#9F62F2] bg-clip-text text-transparent after:content-[''] after:absolute after:left-0 after:bottom-[0px] after:w-full after:h-[1px] after:bg-gradient-to-r after:from-[#632EE3] after:to-[#9F62F2]" : "text-black"}
+                    relative text-[16px] cursor-pointer font-medium ${isActive ? "bg-linear-to-r from-[#632EE3] to-[#9F62F2] bg-clip-text text-transparent after:content-[''] after:absolute after:left-0 after:bottom-0 after:w-full after:h-px after:bg-linear-to-r after:from-[#632EE3] after:to-[#9F62F2]" : "text-black"}
                 `}>
                         Apps
                     </NavLink>
 
                     <NavLink to="/installation" className={({ isActive }) => `
-                    relative text-[16px] cursor-pointer font-medium ${isActive ? "bg-gradient-to-r from-[#632EE3] to-[#9F62F2] bg-clip-text text-transparent after:content-[''] after:absolute after:left-0 after:bottom-[0px] after:w-full after:h-[1px] after:bg-gradient-to-r after:from-[#632EE3] after:to-[#9F62F2]" : "text-black"}
+                    relative text-[16px] cursor-pointer font-medium ${isActive ? "bg-linear-to-r from-[#632EE3] to-[#9F62F2] bg-clip-text text-transparent after:content-[''] after:absolute after:left-0 after:bottom-0 after:w-full after:h-px after:bg-linear-to-r after:from-[#632EE3] after:to-[#9F62F2]" : "text-black"}
                 `}>
                         Installations
                     </NavLink>
-                    <NavLink to="/login" className="flex gap-[10px] w-full max-w-[145px] h-[43px] justify-center items-center bg-linear-to-r from-[#632EE3] to-[#9F62F2] rounded-[4px] cursor-pointer">Login</NavLink>
-                    <NavLink to="/register" className="flex gap-[10px] w-full max-w-[145px] h-[43px] justify-center items-center bg-linear-to-r from-[#632EE3] to-[#9F62F2] rounded-[4px] cursor-pointer">Register</NavLink>
+                    <NavLink to="/login" className="flex gap-[10px] w-full max-w-[145px] h-[43px] justify-center items-center bg-linear-to-r from-[#632EE3] to-[#9F62F2] rounded-[4px] cursor-pointer text-white">Login</NavLink>
+                    <NavLink to="/register" className="flex gap-[10px] w-full max-w-[145px] h-[43px] justify-center items-center bg-linear-to-r from-[#632EE3] to-[#9F62F2] rounded-[4px] cursor-pointer text-white">Register</NavLink>
                 </ul>
 
             </nav>
--- src/Pages/ErrorPage.jsx
+++ src/Pages/ErrorPage.jsx
@@ -1,9 +1,14 @@
 import React from 'react';
+import Error from '../assets/Error.png';
+import { Link } from 'react-router';
 
 const ErrorPage = () => {
     return (
-        <div>
-            Error
+        <div className='inter w-full h-auto bg-[#f5f5f5] flex flex-col justify-center items-center py-[80px]'>
+            <img src={Error} className='w-full max-w-[419.62px] h-auto mb-[54.6px]' />
+            <p className='text-[36px] text-[#001931] font-semibold text-center mb-[8px]'>Oops, page not found!</p>
+            <p className='text-[#627382] text-[20px] text-center mb-[16px]'>The page you are looking for is not available.</p>
+            <Link to="/" className="flex gap-[10px] w-full max-w-[145px] h-[43px] justify-center items-center bg-gradient-to-r from-[#632EE3] to-[#9F62F2] rounded-[4px] cursor-pointer text-white font-semibold mb-[80px]">Go Back!</Link>
         </div>
     );
 };
