5b99b29d2c04f95c9d3eba49d80fe527627b3c4f
Samioul51 <akmsamioulislam51@gmail.com>
2025-10-24 20:50:21 +0600

    Profile update implemented

--- src/Pages/MyProfile.jsx
+++ src/Pages/MyProfile.jsx
@@ -7,22 +7,22 @@ const MyProfile = () => {
     console.log(user);
     return (
         <div className='w-full bg-[#f5f5f5]'>
-        <div className='w-full max-w-[1440px] flex flex-col items-center py-[50px] mx-auto'>
-            <p className='text-center text-[32px] font-bold bg-linear-to-r from-[#632EE3] to-[#9F62F2] bg-clip-text text-transparent mb-[10px]'>MY PROFILE</p>
-            <div className="bg-base-200 h-auto bg-white p-[100px] box-border rounded-[10px]">
-                <div className="flex flex-col items-center justify-center gap-[20px] lg:flex-row">
-                    <img
-                        src={user.photoURL}
-                        className="max-w-sm rounded-lg shadow-2xl"
-                    />
-                    <div className='flex flex-col'>
-                        <p className='py-3'><span className='font-bold'>Name: </span>{user.displayName}</p>
-                        <p className='py-3'><span className='font-bold'>Email: </span>{user.email}</p>
-                        <Link to="/updateprofile" className="flex gap-[10px] w-full max-w-[145px] h-[43px] justify-center items-center bg-linear-to-r from-[#632EE3] to-[#9F62F2] rounded-[4px] cursor-pointer text-white mx-auto">Update</Link>
+            <div className='w-full max-w-[1440px] flex flex-col items-center py-[50px] mx-auto'>
+                <p className='text-center text-[32px] font-bold bg-linear-to-r from-[#632EE3] to-[#9F62F2] bg-clip-text text-transparent mb-[10px]'>MY PROFILE</p>
+                <div className="w-full max-w-[800px] h-auto bg-white p-[100px] box-border rounded-[10px]">
+                    <div className="flex flex-col items-center justify-center gap-[20px] lg:flex-row">
+                        <img
+                            src={user.photoURL}
+                            className="w-full max-w-[100px] h-auto rounded-lg shadow-2xl"
+                        />
+                        <div className='flex flex-col'>
+                            <p className='py-3'><span className='font-bold'>Name: </span>{user.displayName}</p>
+                            <p className='py-3'><span className='font-bold'>Email: </span>{user.email}</p>
+                            <Link to="/updateprofile" className="flex gap-[10px] w-full max-w-[145px] h-[43px] justify-center items-center bg-linear-to-r from-[#632EE3] to-[#9F62F2] rounded-[4px] cursor-pointer text-white mx-auto">Update</Link>
+                        </div>
                     </div>
                 </div>
             </div>
-        </div>
         </div>
     );
 };
--- src/Pages/UpdateProfile.jsx
+++ src/Pages/UpdateProfile.jsx
@@ -1,9 +1,63 @@
-import React from 'react';
+import React, { use, useState } from 'react';
+import { AuthContext } from '../Provider/AuthProvider';
+import { toast } from 'react-toastify';
+import { useNavigate } from 'react-router';
+
 
 const UpdateProfile = () => {
+    const { user, updateUser, setUser } = use(AuthContext);
+    const [options, setOptions] = useState("");
+    const navigate = useNavigate();
+
+    const handleUpdate = (e) => {
+        e.preventDefault();
+        // console.log(user);
+        const form = e.target;
+        const name = form.name.value;
+        const photo = form.photo.value;
+
+        const updatedData = {};
+        if (name) 
+            updatedData.displayName = name;
+        if (photo) 
+            updatedData.photoURL = photo;
+
+        if (!name && !photo) {
+            setOptions("Fill at least one field to update.");
+            return;
+        }
+        else
+            setOptions("");
+
+        updateUser(updatedData).then(() => {
+            setUser({ ...user, displayName: name || user.displayName, photoURL: photo || user.photoURL });
+            toast("Profile updated successfully!")
+            navigate("/");
+        }).catch((error) => {
+            toast("Failed to update profile: ", error.message);
+        });
+
+    }
+
     return (
-        <div>
-            
+        <div className='w-full bg-[#f5f5f5]'>
+            <div className='w-full max-w-[1440px] flex flex-col items-center py-[50px] mx-auto'>
+                <p className='text-center text-[32px] font-bold bg-linear-to-r from-[#632EE3] to-[#9F62F2] bg-clip-text text-transparent mb-[10px]'>UPDATE PROFILE</p>
+                <div className="w-full max-w-[500px] h-auto bg-white p-[100px] box-border rounded-[10px]">
+                    <form onSubmit={handleUpdate} className="w-full">
+                        <fieldset className="fieldset">
+                            <label className="label">Name</label>
+                            <input name="name" type="text" className="input" placeholder="Name" />
+                            <label className="label">Photo URL</label>
+                            <input name="photo" type="URL" className="input" placeholder="Photo URL" />
+                            {
+                                options && <p className='text-xs text-red-500'>{options}</p>
+                            }
+                            <button type="submit" className="mx-auto border-0 mt-4 w-full max-w-[145px] h-[43px] justify-center items-center bg-linear-to-r from-[#632EE3] to-[#9F62F2] rounded-[4px] cursor-pointer text-white ">Confirm Update</button>
+                        </fieldset>
+                    </form>
+                </div>
+            </div>
         </div>
     );
 };
